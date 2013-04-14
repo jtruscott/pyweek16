269ef3d5c5ff2361f121e076336341d362daa7e4
jtruscott <jesset@ruscott.com>
2013-04-14 11:19:12 -0700

    start putting together an engine. this is more complicated than I usually do, but it might make unit tests possible

--- Console/game.py
+++ Console/game.py
@@ -1,11 +1,104 @@
 import event
+import time
 import pytality
+import Queue as queue
+import os
+import threading
+import traceback
+import sys
 import logging
 
 log = logging.getLogger(__name__)
 
-class GameShutdown(Exception): pass
+class GameShutdown(Exception):
+    pass
+
+Stop = object()
+
+ticks = 0
+target_fps = 15
+
+# in previous compos i'd just take a lock and run stuff on the tick thread
+# this time i'm going to try pushing stuff onto a queue so I can maybe make unit tests
+action_queue = queue.Queue()
+tick_thread = None
+shutdown_event = threading.Event()
+
+mode = "level"
 
 def start():
     log.debug("Starting Game")
-    pytality.term.getkey()
+
+    global tick_thread
+    tick_thread = threading.Thread(target=tick_loop, name="tick_loop")
+    tick_thread.daemon = True
+    tick_thread.start()
+
+    global input_thread
+    input_thread = threading.Thread(target=input_loop, name="input_loop")
+    input_thread.daemon = True
+    input_thread.start()
+
+    try:
+        while True:
+            kwargs = action_queue.get()
+            if kwargs is Stop:
+                raise GameShutdown()
+
+            event_name = kwargs.pop('type')
+            event.fire('%s.%s' % (mode, event_name), **kwargs)
+            event.fire('%s.predraw' % mode)
+            event.fire('%s.draw' % mode)
+            pytality.term.flip()
+
+    except KeyboardInterrupt as e:
+        raise GameShutdown()
+
+
+def tick_loop():
+    global ticks
+    delay = 1.0 / target_fps
+    try:
+        log.debug("Tick thread started")
+        while True:
+            action_queue.put(dict(type='tick'))
+            ticks += 1
+
+            if shutdown_event.wait(delay):
+                #we've been asked to shut dow
+                log.debug("Tick thread shutting down")
+                return
+    
+    except KeyboardInterrupt as e:
+        action_queue.put(Stop)
+
+    except Exception as e:
+        logging.exception(e)
+        try:
+            traceback.print_exc(e, file=sys.stderr)
+        finally:
+            os._exit(1)
+            return
+
+def input_loop():
+    try:
+        log.debug("Input thread started")
+        while True:
+            k = pytality.term.getkey()
+            if shutdown_event.is_set():
+                log.debug("Tick thread shutting down")
+                return
+
+            if k:
+                action_queue.put(dict(type='input', key=k.lower()))
+
+    except KeyboardInterrupt as e:
+        action_queue.put(Stop)
+
+    except Exception as e:
+        logging.exception(e)
+        try:
+            traceback.print_exc(e, file=sys.stderr)
+        finally:
+            os._exit(1)
+            return
--- Console/main.py
+++ Console/main.py
@@ -7,11 +7,12 @@ import logging
 log = logging.getLogger(__name__)
 log.debug("Starting up")
 
-screen_width = 100
-screen_height = 80
+# At least one user on the IRC is at 1280x800, which with 12px tall rows limits us to 65 max.
+screen_width = 155
+screen_height = 65
 
 def main():
-    # For compatability reasons, i'm going to force pytality to be SDL only  for this compo.
+    # For compatability reasons, i'm going to force pytality to be SDL only for this compo.
     pytality.term.init(backends=['pygame'], width=screen_width, height=screen_height)
     
     pytality.term.set_title('No Title Yet (PyWeek #16)')
@@ -34,3 +35,8 @@ def main():
         log.debug('Shutting down')
         logging.shutdown()
         pytality.term.reset()
+        if game.tick_thread:
+            game.shutdown_event.set()
+            # it really doesn't matter that we join these, it just suppresses errors
+            game.tick_thread.join(timeout=0.1)
+            game.input_thread.join(timeout=0.1)
